94d60b472d5eb6739458b27bd88dc7b0a180d0cb
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-02-19 17:00:39 +0100

    CH20: Implement verifier

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup, Group};
-use ark_ff::{Field, Zero};
+use ark_ff::{Field, One, Zero};
 use ark_std::UniformRand;
 use rand::thread_rng;
 
@@ -56,13 +56,9 @@ fn mul_mat_by_vec<G: Group>(mat: &[Vec<G>], vec: &[G::ScalarField]) -> Vec<G> {
     res
 }
 
-pub struct AlgInst<G: Group> {
-    pub inst: Vec<G>,
-}
+pub struct AlgInst<G: Group>(Vec<G>);
 
-pub struct AlgWit<G: Group> {
-    pub wit: Vec<G::ScalarField>,
-}
+pub struct AlgWit<G: Group>(Vec<G::ScalarField>);
 
 pub struct CH20CRS<P: Pairing> {
     pub e: P::G2,
@@ -96,10 +92,10 @@ where
     let r: Vec<P::ScalarField> = (0..(lang.wit_size()))
         .map(|_i| <P::ScalarField as UniformRand>::rand(&mut rng))
         .collect();
-    let matrix = lang.instantiate_matrix(&inst.inst);
+    let matrix = lang.instantiate_matrix(&inst.0);
     let a: Vec<P::G1> = mul_mat_by_vec(&matrix, &r);
     let d: Vec<P::G2> = wit
-        .wit
+        .0
         .iter()
         .zip(r.iter())
         .map(|(w_i, r_i)| crs.e * w_i + P::G2::generator() * r_i)
@@ -115,8 +111,32 @@ pub fn ch20_verify<P: Pairing>(
     crs: CH20CRS<P>,
     lang: AlgLang<P::G1>,
     inst: AlgInst<P::G1>,
+    proof: CH20Proof<P>,
 ) -> Result<(), CH20VerifierError> {
-    unimplemented!()
+    let mut lhs: Vec<Vec<P::G1>> = vec![];
+    let mut rhs: Vec<Vec<P::G2>> = vec![];
+    let mat = lang.instantiate_matrix(&inst.0);
+    for i in 0..lang.inst_size() {
+        for j in 0..lang.wit_size() {
+            lhs[i].push(mat[i][j]);
+            rhs[i].push(proof.d[j]);
+        }
+        lhs[i].push(inst.0[i]);
+        rhs[i].push(-crs.e);
+        lhs[i].push(proof.a[i]);
+        rhs[i].push(-P::G2::generator());
+    }
+    // TODO: for efficiency, recombine equations first with a random
+    // element, this saves up quite some pairings
+    for (l, r) in lhs.iter().zip(rhs.iter()) {
+        let pairing_res = P::multi_pairing(l, r);
+        if pairing_res != Zero::zero() {
+            return Err(CH20VerifierError::CH20GenericError(From::from(
+                "Pairing is nonzero",
+            )));
+        }
+    }
+    Ok(())
 }
 
 fn main() {

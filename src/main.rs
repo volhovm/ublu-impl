f739d60cdc194608fa60ba12115a1746a7f99dee
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-02-19 16:33:03 +0100

    CH20: add prover/verifier sampling rand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use ark_ec::{pairing::Pairing, CurveGroup, Group};
-use ark_ff::Field;
+use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup, Group};
+use ark_ff::{Field, Zero};
 use ark_std::UniformRand;
 use rand::thread_rng;
 
@@ -14,13 +14,27 @@ pub struct AlgLang<G: Group> {
 }
 
 impl<G: Group> AlgLang<G> {
-    pub fn instantiate_matrix(&self, inst: Vec<G>) -> Vec<Vec<G>> {
+    pub fn instantiate_matrix(&self, inst: &Vec<G>) -> Vec<Vec<G>> {
         unimplemented!()
+    }
+    pub fn wit_size(&self) -> usize {
+        self.matrix[0].len()
+    }
+    pub fn inst_size(&self) -> usize {
+        self.matrix.len()
     }
 }
 
-fn mul_mat_by_vec<G: Group>(mat: Vec<Vec<G>>, vec: Vec<G::ScalarField>) -> Vec<G> {
-    unimplemented!()
+fn mul_mat_by_vec<G: Group>(mat: &[Vec<G>], vec: &[G::ScalarField]) -> Vec<G> {
+    let n = mat.len();
+    let m = mat[0].len();
+    let mut res: Vec<G> = vec![Zero::zero(); n];
+    for i in 0..n {
+        for j in 0..m {
+            res[i] += mat[i][j] * vec[j];
+        }
+    }
+    res
 }
 
 pub struct AlgInst<G: Group> {
@@ -37,14 +51,16 @@ pub struct CH20CRS<P: Pairing> {
 
 pub struct CH20Proof<P: Pairing> {
     pub a: Vec<P::G1>,
-    pub b: Vec<P::G2>,
+    pub d: Vec<P::G2>,
 }
 
-pub fn ch20_setup<P: Pairing>() -> CH20CRS<P> {
+pub fn ch20_setup<P: Pairing>() -> CH20CRS<P>
+where
+    P::ScalarField: UniformRand,
+{
     let mut rng = thread_rng();
-    //let e = P::ScalarField::rand(&mut rng);
-    //let e = <P as Pairing>::G2::rand(&mut rng);
-    let e = unimplemented!();
+    let e_td: P::ScalarField = <P::ScalarField as UniformRand>::rand(&mut rng);
+    let e: P::G2 = P::G2::generator() * e_td;
     CH20CRS { e }
 }
 
@@ -53,16 +69,23 @@ pub fn ch20_prove<P: Pairing>(
     lang: AlgLang<P::G1>,
     inst: AlgInst<P::G1>,
     wit: AlgWit<P::G1>,
-) -> CH20Proof<P> {
-    let r: Vec<P::ScalarField> = unimplemented!();
-    let matrix = lang.instantiate_matrix(inst.inst);
-    let a: Vec<P::G1> = mul_mat_by_vec(matrix, r);
-    //let d: Vec<P::G2> = wit
-    //    .wit
-    //    .iter()
-    //    .zip(r.iter())
-    //    .map(|(w_i, r_i)| crs.e * w_i + r_i);
-    unimplemented!()
+) -> CH20Proof<P>
+where
+    P::ScalarField: UniformRand,
+{
+    let mut rng = thread_rng();
+    let r: Vec<P::ScalarField> = (0..(lang.wit_size()))
+        .map(|_i| <P::ScalarField as UniformRand>::rand(&mut rng))
+        .collect();
+    let matrix = lang.instantiate_matrix(&inst.inst);
+    let a: Vec<P::G1> = mul_mat_by_vec(&matrix, &r);
+    let d: Vec<P::G2> = wit
+        .wit
+        .iter()
+        .zip(r.iter())
+        .map(|(w_i, r_i)| crs.e * w_i + P::G2::generator() * r_i)
+        .collect();
+    CH20Proof { a, d }
 }
 
 pub enum CH20VerifierError {

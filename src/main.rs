930175beb5d0a7a3273bae5fa05f9277d0b1a5a2
Mikhail Volkhov <volhovm.cs@gmail.com>
2024-05-01 22:35:41 +0100

    Adding consistency transformation matrix

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ use ark_ff::{One, PrimeField, Zero};
 use ark_std::UniformRand;
 use rand::thread_rng;
 
+fn binomial(n: usize, k: usize) -> usize {
+    if k == 0 {
+        1
+    } else {
+        (n * binomial(n - 1, k - 1)) / k
+    }
+}
+
 fn field_pow<F: PrimeField>(base: F, exp: usize) -> F {
     let mut res: F = F::one();
     let mut exp2 = exp;
@@ -230,32 +238,38 @@ pub fn ch20_verify<P: Pairing>(
 
 pub struct CH20Trans<P: Pairing> {
     pub t_am: Vec<Vec<P::ScalarField>>,
-    pub t_aa: Vec<P::ScalarField>,
+    pub t_aa: Vec<P::G1>,
     pub t_xm: Vec<Vec<P::ScalarField>>,
-    pub t_xa: Vec<P::ScalarField>,
+    pub t_xa: Vec<P::G1>,
     pub t_wm: Vec<Vec<P::ScalarField>>,
     pub t_wa: Vec<P::ScalarField>,
 }
 
 impl<P: Pairing> CH20Trans<P> {
     /// Creates a zero (ID) transformation for a given language.
-    pub fn zero_trans(lang: &AlgLang<P::G1>) -> CH20Trans<CC> {
-        let mut t_xm: Vec<Vec<CF>> = vec![vec![CF::zero(); lang.inst_size()]; lang.inst_size()];
+    pub fn zero_trans(lang: &AlgLang<P::G1>) -> CH20Trans<P> {
+        let mut t_xm: Vec<Vec<P::ScalarField>> =
+            vec![vec![P::ScalarField::zero(); lang.inst_size()]; lang.inst_size()];
         for i in 0..lang.inst_size() {
-            t_xm[i][i] = CF::one();
+            t_xm[i][i] = P::ScalarField::one();
         }
-        let mut t_wm: Vec<Vec<CF>> = vec![vec![CF::zero(); lang.wit_size()]; lang.wit_size()];
+        let mut t_wm: Vec<Vec<P::ScalarField>> =
+            vec![vec![P::ScalarField::zero(); lang.wit_size()]; lang.wit_size()];
         for i in 0..lang.wit_size() {
-            t_wm[i][i] = CF::one();
+            t_wm[i][i] = P::ScalarField::one();
         }
 
-        let t_xa: Vec<CF> = vec![CF::zero(); lang.inst_size()];
-        let t_wa: Vec<CF> = vec![CF::zero(); lang.wit_size()];
+        let t_xa: Vec<P::G1> = vec![P::G1::zero(); lang.inst_size()];
+        let t_wa: Vec<P::ScalarField> = vec![P::ScalarField::zero(); lang.wit_size()];
 
-        let t_am: Vec<Vec<CF>> = t_xm
+        let t_am: Vec<Vec<P::ScalarField>> = t_xm
             .clone()
             .into_iter()
-            .map(|row| row.into_iter().chain(t_xa.clone()).collect())
+            .map(|row| {
+                row.into_iter()
+                    .chain(vec![P::ScalarField::zero(); lang.inst_size()])
+                    .collect()
+            })
             .collect();
         let t_aa = t_xa.clone();
 
@@ -271,12 +285,7 @@ impl<P: Pairing> CH20Trans<P> {
 
     pub fn update_instance(&self, inst: &AlgInst<P::G1>) -> AlgInst<P::G1> {
         let inst_prime_e1: Vec<P::G1> = mul_mat_by_vec_f_g(&self.t_xm, &inst.0);
-        let inst_prime_e2: Vec<P::G1> = self
-            .t_xa
-            .clone()
-            .into_iter()
-            .map(|x| P::G1::generator() * x)
-            .collect();
+        let inst_prime_e2: Vec<P::G1> = self.t_xa.clone();
 
         let inst_prime: Vec<P::G1> = inst_prime_e1
             .into_iter()
@@ -350,12 +359,7 @@ pub fn ch20_update<P: Pairing>(
             .chain(inst.0.clone())
             .collect::<Vec<P::G1>>(),
     );
-    let a_prime_e2: Vec<P::G1> = trans
-        .t_aa
-        .clone()
-        .into_iter()
-        .map(|x| P::G1::generator() * x)
-        .collect();
+    let a_prime_e2: Vec<P::G1> = trans.t_aa.clone();
 
     let inst_prime = trans.update_instance(inst);
     let mat_prime = lang.instantiate_matrix(&inst_prime.0);
@@ -435,7 +439,7 @@ fn test_ch20_correctness() {
             vec![CF::zero(), delta, CF::zero()],
             vec![CF::zero(), CF::zero(), gamma * delta],
         ];
-        let t_xa: Vec<CF> = vec![CF::zero(); 3];
+        let t_xa: Vec<CG1> = vec![CG1::zero(); 3];
         let t_wm: Vec<Vec<CF>> = vec![vec![gamma, CF::zero()], vec![CF::zero(), delta]];
         let t_wa: Vec<CF> = vec![CF::zero(); 2];
         let emptyrow: Vec<CF> = vec![CF::zero(); 3];
@@ -474,7 +478,9 @@ pub fn test_ublu_consistency<P: Pairing>() {
     let g: P::G1 = UniformRand::rand(&mut rng);
 
     let d = 4;
+    // Number of instance elements
     let n = 3 * d + 4;
+    // Number of witness elements
     let m = 2 * d + 8;
 
     let hs: Vec<_> = (0..d + 2)
@@ -482,10 +488,7 @@ pub fn test_ublu_consistency<P: Pairing>() {
         .collect();
 
     let matrix = {
-        // g 0
-        // 0 g
-        // 0 x1
-        let mut matrix: Vec<Vec<LinearPoly<P::G1>>> = vec![vec![LinearPoly::zero(4); m]; n];
+        let mut matrix: Vec<Vec<LinearPoly<P::G1>>> = vec![vec![LinearPoly::zero(n); m]; n];
 
         matrix[0][0] = LinearPoly::constant(n, g);
         matrix[0][1] = LinearPoly::constant(n, hs[0]);
@@ -583,10 +586,112 @@ pub fn test_ublu_consistency<P: Pairing>() {
 
     let lang_valid = lang.contains(&inst, &wit);
     println!("Language valid? {lang_valid:?}");
+
+    // Instance: Tcal,Xcal,Acal,[(Ai,Di)] for 1..d, 1,1,[1..1] for 1..d-1
+    // Witness: t,r_t,x,r_x,alpha,r_alpha,x-t,alpha*(x-t),r_alpha*(x-t),[r_i] for 1..d,[r_i*(x-t)] for 1..d-1
+    let _trans: CH20Trans<P> = {
+        let u_x: P::ScalarField = UniformRand::rand(&mut rng);
+        let u_rx: P::ScalarField = UniformRand::rand(&mut rng);
+        let u_alpha: P::ScalarField = UniformRand::rand(&mut rng);
+        let u_ralpha: P::ScalarField = UniformRand::rand(&mut rng);
+        let u_rs: Vec<P::ScalarField> = (0..d).map(|_i| UniformRand::rand(&mut rng)).collect();
+
+        let mut t_xm: Vec<Vec<P::ScalarField>> = vec![vec![P::ScalarField::zero(); n]; n];
+        let mut t_xa: Vec<P::G1> = vec![P::G1::zero(); m];
+        let mut t_wm: Vec<Vec<P::ScalarField>> = vec![vec![P::ScalarField::zero(); m]; m];
+        let mut t_wa: Vec<P::ScalarField> = vec![P::ScalarField::zero(); m];
+
+        for i in 0..n {
+            t_xm[i][i] = P::ScalarField::one();
+        }
+        for i in 0..m {
+            t_wm[i][i] = P::ScalarField::one();
+        }
+
+        t_wa[2] = u_x; // x + U_x
+        t_wa[3] = u_rx; // rx + U_rx
+        t_wa[4] = u_alpha; // α + U_α
+        t_wa[5] = u_ralpha; // rα + U_rα
+        t_wa[6] = u_x; // (x-t) + U_x
+        t_wm[7][4] = u_x; // α*(x-t) + α*U_x
+        t_wm[7][6] = u_alpha; //         + (x-t)*U_α
+        t_wa[7] = u_x * u_alpha; // + U_x*U_α
+        t_wm[8][5] = u_x; // rα*(x-t) + rα*U_x
+        t_wm[8][6] = u_ralpha; //         + (x-t)*U_rα
+        t_wa[8] = u_x * u_ralpha; // + U_x*U_α
+
+        let v_coeff = |i: usize, j: usize, x: P::ScalarField| {
+            field_pow(x, i - j) * P::ScalarField::from(binomial(i, j) as u64)
+        };
+
+        for i in 0..d {
+            for j in 0..i + 1 {
+                t_wm[9 + i][9 + j] = v_coeff(i + 1, j + 1, u_x)
+            }
+            t_wa[9 + i] = u_rs[i]
+        }
+        for i in 0..d - 1 {
+            for j in 0..i + 1 {
+                t_wm[9 + d + i][9 + d + j] = v_coeff(i + 1, j + 1, u_x);
+                t_wm[9 + d + i][9 + j] = u_x * v_coeff(i + 1, j + 1, u_x);
+            }
+            t_wm[9 + d + i][6] = u_rs[i];
+            t_wa[9 + d + i] = u_rs[i] * u_x;
+        }
+
+        t_xa[1] = g * u_x + hs[0] * u_rx; // Xcal * G^{U_x} * H0^{U_rx}
+        t_xa[2] = g * u_alpha + hs[0] * u_ralpha; // Acal * G^{U_α} * H0^{U_rα}
+
+        for i in 0..d {
+            for j in 0..(i + 1) {
+                t_xm[3 + 2 * i][3 + 2 * j] = v_coeff(i + 1, j + 1, u_x);
+                t_xm[4 + 2 * i][4 + 2 * j] = v_coeff(i + 1, j + 1, u_x);
+            }
+            t_xa[3 + 2 * i] = g * u_rs[i];
+            // TODO what are these ws[4]???
+            //let sumterm: P::G1 = (0..(i + 1))
+            //    .map(|j| -hs[2 + j] * (ws[4] * v_coeff(i + 1, j + 1, u_x)))
+            //    .fold(P::ScalarField::zero(), |x, y| x + y);
+            //t_xa[4 + 2 * i] = g * (field_pow(u_x, i + 1))
+            //    + hs[2 + i] * (ws[4] + u_alpha)
+            //    + sumterm
+            //    + hs[1] * u_rs[i];
+        }
+
+        //
+        // //T_reduce_map = {var('U_α'): 0, var('w_α'): 0}
+        // T_reduce_map = {}
+        // Tx1 = Matrix(subs_mat(t_xm,T_reduce_map))
+        // Tx2 = vector(subs_vec(t_xa,T_reduce_map))
+        // print((Tx2[6]))
+        // Tw1 = Matrix(subs_mat(t_wm,T_reduce_map))
+        // Tw2 = vector(subs_vec(t_wa,T_reduce_map))
+        //
+
+        let t_am: Vec<Vec<P::ScalarField>> = t_xm
+            .clone()
+            .into_iter()
+            .map(|row| {
+                row.into_iter()
+                    .chain(vec![P::ScalarField::zero(); m])
+                    .collect()
+            })
+            .collect();
+        let t_aa = t_xa.clone();
+
+        CH20Trans {
+            t_am,
+            t_aa,
+            t_xm,
+            t_xa,
+            t_wm,
+            t_wa,
+        }
+    };
 }
 
 fn main() {
-    test_field_pow();
+    //test_field_pow();
     test_ublu_consistency::<CC>();
-    test_ch20_correctness();
+    //test_ch20_correctness();
 }
